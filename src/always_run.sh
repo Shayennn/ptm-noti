89cf3988fdf4f4097c40380c856198bd4203a6fe
Shayennn <8080856+Shayennn@users.noreply.github.com>
2024-12-21 12:18:10 +0700

    chore: better always_run.sh

--- src/always_run.sh
+++ src/always_run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-while true
-do
+trap "echo 'SIGTERM received, exiting...'; exit 0" SIGTERM
+
+while true; do
     # try pypy3 first if it exists
-    if command -v pypy3 &> /dev/null
-    then
+    if command -v pypy3 &>/dev/null; then
         pypy3 main.py
     else
         python3 main.py
     fi
-    sleep 1800
-    if [ $? -eq 130 ]
-    then
-        break
-    fi
+    sleep 1800 &
+    wait $!
 done
