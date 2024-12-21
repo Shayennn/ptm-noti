ebd368023d5eefd9b79c074c0202c828a8cdfa16
Shayennn <8080856+Shayennn@users.noreply.github.com>
2024-12-21 12:30:01 +0700

    fix: remove SIG

--- src/always_run.sh
+++ src/always_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trap "echo 'SIGTERM received, exiting...'; exit 0" SIGTERM
+trap "echo 'SIGTERM received, exiting...'; exit 0" TERM
 
 while true; do
     # try pypy3 first if it exists
